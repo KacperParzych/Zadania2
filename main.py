3ef6dd7055727ea54c197b70a489320ff60d64e9
KacperParzych <175176@student.uwm.edu.pl>
2024-03-10 14:53:17 +0100

    1.0

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 #1
 zbior1 = {1-i: i for i in range(1, 10)}
 print(zbior1)
@@ -9,3 +10,15 @@ for i in zbior2:
         zbior3.append(i)
 print(zbior3)
 #2
+lista1 = []
+for i in range(0, 10):
+    A = random.randint(1, 100)
+    lista1.append(A)
+print(lista1)
+
+lista2 = []
+for i in lista1:
+    if i % 2 == 0:
+        lista2.append(i)
+print(lista2)
+
